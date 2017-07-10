a42a8c65894f1e47ef9384c63db308c2330ed8f0
Andrzej Frankowski <andrewfrankowski@gmail.com>
2017-07-10 02:12:52 +0100

    Pimp EventTableViewCell

--- Sportivity/Common/ViewModels/EventViewModel.swift
+++ Sportivity/Common/ViewModels/EventViewModel.swift
@@ -14,6 +14,26 @@ import RxCocoa
 
 class EventViewModel : IdentifiableType, Equatable {
     
+    fileprivate static var dayFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d"
+        return formatter
+    }()
+    
+    fileprivate static var monthFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM"
+        return formatter
+    }()
+    
+    fileprivate static var hourFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .none
+        formatter.timeStyle = .short
+        
+        return formatter
+    }()
+    
     let event: Event
     
     typealias Identity = String
@@ -24,6 +44,11 @@ class EventViewModel : IdentifiableType, Equatable {
     
     let name: Driver<String>
     let photoUrl: Driver<URL?>
+    let address: Driver<String?>
+    let attendeesCount: Driver<Int?>
+    let month: Driver<String?>
+    let day: Driver<String?>
+    let hour: Driver<String?>
 
     init(event: Event) {
         self.event = event
@@ -37,6 +62,22 @@ class EventViewModel : IdentifiableType, Equatable {
         let eventPhoto = event.photoUrl.asDriver()
         photoUrl = Driver.combineLatest(eventPhoto, placePhoto, resultSelector: { (eventPhoto, placePhoto) in
             return eventPhoto ?? placePhoto
+        })
+        address = event.place
+            .asObservable()
+            .flatMap { (place) -> Observable<String?> in
+                return place?.street.asObservable() ?? Observable<String?>.just(nil)
+            }
+            .asDriver(onErrorJustReturn: nil)
+        attendeesCount = event.attendees.asDriver().map { $0.count }
+        month = event.dateStart.asDriver().map({ (date) -> String? in
+            return EventViewModel.monthFormatter.string(from: date).uppercased()
+        })
+        day = event.dateStart.asDriver().map({ (date) -> String? in
+            return EventViewModel.dayFormatter.string(from: date).uppercased()
+        })
+        hour = event.dateStart.asDriver().map({ (date) -> String? in
+            return EventViewModel.hourFormatter.string(from: date)
         })
     }
 }
--- Sportivity/Common/Views/EventCell/EventTableViewCell.swift
+++ Sportivity/Common/Views/EventCell/EventTableViewCell.swift
@@ -17,7 +17,11 @@ class EventTableViewCell: UITableViewCell, Configurable {
 
     @IBOutlet fileprivate weak var photoImageView: UIImageView!
     @IBOutlet fileprivate weak var nameLabel: UILabel!
-    
+    @IBOutlet fileprivate weak var addressLabel: UILabel!
+    @IBOutlet fileprivate weak var attendeesLabel: UILabel!
+    @IBOutlet fileprivate weak var monthLabel: UILabel!
+    @IBOutlet fileprivate weak var dayLabel: UILabel!
+
     var viewModel: EventViewModel!
     
     override func prepareForReuse() {
@@ -35,6 +39,40 @@ class EventTableViewCell: UITableViewCell, Configurable {
             .driveNext { [unowned self] (url) in
                 self.photoImageView.kf.setImage(with: url)
             }
+            .addDisposableTo(reuseBag)
+        Observable
+            .combineLatest(viewModel.address.asObservable(), viewModel.hour.asObservable(), viewModel.attendeesCount.asObservable()) { (address, hour, attendingCount) -> String in
+                var text : String = address != nil ? address! : ""
+                if text.isEmpty {
+                    text = hour ?? ""
+                } else if let hourText = hour, !hourText.isEmpty {
+                    text += " • at \(hourText)"
+                }
+                guard let count = attendingCount, count > 0 else { return text }
+                return "\(text) • \(count) attending"
+            }
+            .asDriver(onErrorJustReturn: "")
+            .drive(addressLabel.rx.text)
+            .addDisposableTo(reuseBag)
+        viewModel
+            .address
+            .drive(addressLabel.rx.text)
+            .addDisposableTo(reuseBag)
+        viewModel
+            .attendeesCount
+            .map { count -> String? in
+                guard let count = count, count > 0 else { return nil }
+                return "\(count) attending"
+            }
+            .drive(attendeesLabel.rx.text)
+            .addDisposableTo(reuseBag)
+        viewModel
+            .month
+            .drive(monthLabel.rx.text)
+            .addDisposableTo(reuseBag)
+        viewModel
+            .day
+            .drive(dayLabel.rx.text)
             .addDisposableTo(reuseBag)
     }
 }
